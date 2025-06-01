5c813b84b646641655a52bbdea6f6833053aba37
Gustavo Oliveira <gustavo28064212@gmail.com>
2025-05-31 22:36:40 -0300

    correct the offset in VRAM

--- src/main/java/ppu/VRAM.java
+++ src/main/java/ppu/VRAM.java
@@ -1,14 +1,16 @@
 package ppu;
 
-// VRAM.java
 public class VRAM {
-    private final int[] vramData = new int[0x2000];
+    private final int[] vramData = new int[0x2000]; // 8KB of VRAM
 
     public int read(int address) {
-        return vramData[address];
+        // Map PPU address to internal array index
+        // e.g., PPU 0x2000 -> vramData[0], PPU 0x2FFF -> vramData[0xFFF], PPU 0x3FFF -> vramData[0x1FFF]
+        return vramData[address & 0x1FFF];
     }
 
     public void write(int address, int value) {
-        vramData[address] = value;
+        // Map PPU address to internal array index
+        vramData[address & 0x1FFF] = value;
     }
 }
